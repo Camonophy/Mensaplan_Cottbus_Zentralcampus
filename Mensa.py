26d4bcdcc46228cbb6384c180bcebc1c3ec9ec77
pflugoli <pflugoli@b-tu.de>
2021-07-08 13:06:52 +0200

    Implement option to get today's food

--- Mensa.py
+++ Mensa.py
@@ -1,4 +1,6 @@
 import urllib.request, urllib.error, urllib.parse
+import datetime
+
 from bs4 import BeautifulSoup
 
 '''
@@ -16,26 +18,6 @@ class Mensaplan:
     def __init__(self):
         self.__URL = "https://www.swffo.de/2011/ClassPackage/swffo-2020/swffo-speiseplaene/4frame-Speiseplan.CottbusBTU.php"
         self.__speiseplan = {}
-        
-
-    '''
-        Erstelle einen Mensaplan, basierend auf der definierten URL im Konstruktor
-
-        param self: Verweis auf eigene Klasse, kann mit 
-                    [Mensaplan-Objekt].erstelle_speiseplan() 
-                    aufgerufen werden.
-    '''
-    def erstelle_speiseplan(self):
-        response   = urllib.request.urlopen(self.__URL)
-        webContent = response.read()
-        fullPage   = BeautifulSoup(webContent , 'html.parser')
-        tagesMenus = fullPage.findAll(class_="essenAll")
-
-        for menu in tagesMenus:
-            tag      = self.__get_Tag(str(menu.find(class_="speiseplanTag")))
-            gerichte = self.__get_Essen(self.__entferne_Tags(str(menu.findAll(class_="essen"))).split(','))
-
-            self.__speiseplan[tag] = gerichte
 
 
     '''
@@ -105,34 +87,90 @@ class Mensaplan:
     def __get_Tag(self, tag):
         return tag.split("<")[2].split(">")[1]
 
+    
+    '''
+        Liefert das Datum des heutigen tages zurück.
 
+        return: "[Wochentag] [Tag].[Monat]" 
+    '''
+    def __get_tag_heute(self):
+        heute = datetime.datetime.now()
+        heute = datetime.datetime.strftime(heute, '%d.%m')
+        tage  = self.__speiseplan.keys()
+        return [ tag for tag in tage if tag[len(tag)-6: len(tag)-1] == heute ][0]
+
+
+    '''
+        Erstelle einen Mensaplan, basierend auf der definierten URL im Konstruktor
+
+        param self: Verweis auf eigene Klasse, kann mit 
+                    [Mensaplan-Objekt].erstelle_speiseplan() 
+                    aufgerufen werden.
+    '''
+    def erstelle_speiseplan(self):
+        response   = urllib.request.urlopen(self.__URL)
+        webContent = response.read()
+        fullPage   = BeautifulSoup(webContent , 'html.parser')
+        tagesMenus = fullPage.findAll(class_="essenAll")
+
+        for menu in tagesMenus:
+            tag      = self.__get_Tag(str(menu.find(class_="speiseplanTag")))
+            gerichte = self.__get_Essen(self.__entferne_Tags(str(menu.findAll(class_="essen"))).split(','))
+
+            self.__speiseplan[tag] = gerichte
+
+    
     '''
         Gib den Speiseplan aus.
-        Sollte kein Plan erstellt worden sein, wir ein leeres
+        Sollte kein Plan erstellt worden sein, wird ein leeres
         Dictionary zurueckgegeben.
 
-        param self: Verweis auf eigene Klasse, wordurch diese mit
-                    [Mensaplan-Objekt].get_speiseplan() aufgerufen werden.
-        return:     Privates Dictionary, welches den Mensaplan enthaelt
+        param self:  Verweis auf eigene Klasse, wordurch diese mit
+                     [Mensaplan-Objekt].get_speiseplan() aufgerufen werden.
+        param heute: Nur die Liste des Speisen des heutigen Tages
+        return:      Privates Dictionary, welches den Mensaplan enthaelt
     '''
-    def get_speiseplan(self):
-        return self.__speiseplan
+    def get_speiseplan(self, heute=1):
+        return self.__speiseplan[self.__get_tag_heute()] if heute else self.__speiseplan 
+
+    
+    '''
+        Gib den Speiseplan aus in einem lesbaren Format aus.
+
+        param self:    Verweis auf eigene Klasse, wordurch diese mit
+                       [Mensaplan-Objekt].print_speiseplan() aufgerufen werden.
+        param sprache: 0 = Deutsch ; 1 = Englisch
+        param heute:   Nur das Essen des heutigen tages anzeigen
+    '''
+    def print_speiseplan(self, sprache=0, heute=0):
+        if heute:
+            tag = self.__get_tag_heute()
+            plan = self.__speiseplan[tag]
+            ausgabe = [ "Am " + tag + " gibt es folgendes zur Auswahl: ", \
+                        "There is the following to eat on " + tag + ": " ]
+            print(ausgabe[sprache])
+            for name, inhalt in plan.items():
+                print(name + ": ", end="")
+                print(inhalt[sprache])
+            print()
+        else:
+            for tag, essen in self.__speiseplan.items():
+                ausgabe = [ "Am " + tag + " gibt es folgendes zur Auswahl: ", \
+                            "There is the following to eat on " + tag + ": " ] 
+                print(ausgabe[sprache])
+                for name, inhalt in essen.items():
+                    print(name + ": ", end="")
+                    print(inhalt[sprache])
+                print()
+
 
 # Starte diese Datei als Main-File
 if __name__ == "__main__":
     mensaplan = Mensaplan()
     mensaplan.erstelle_speiseplan()
-    speiseplan = mensaplan.get_speiseplan()
-
+    
     sprache = input("Ausgabe auf englisch? / Output in english? ") in ["y", "yes", "j", "ja"]
+    heute = input("Nur das Essen von heute? / Just today's food list? ") in ["y", "yes", "j", "ja"]
+    print()
 
-    for tag, essen in speiseplan.items():
-        ausgabe = [ "Am " + tag + " gibt es folgendes zur Auswahl: ", \
-                    "There is the following to eat on " + tag + ": " ] 
-        print(ausgabe[sprache])
-        for name, inhalt in essen.items():
-            print(name + ": ", end="")
-            print(inhalt[sprache])
-        print()
-
-
+    mensaplan.print_speiseplan(sprache, heute)
